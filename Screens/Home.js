347646d40c0d4f1f481c573e163ac5ae235fd3d6
Huỳnh Bùi Thanh Tân <thanhtan@Huynhs-MacBook-Pro.local>
2023-06-12 17:19:14 +0700

    up tu macbook

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,84 +1,81 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const HomeScreen = () => {
-   const navigation = useNavigation(); 
-   return(
-    <View>
-      
-          <Text>Man hinh Home</Text>
-          <TouchableOpacity style={{backgroundColor:'blue'}}
-           onPress={()=> navigation.replace('Login')}
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Text>Man hinh Home</Text>
+      <TouchableOpacity
+        style={{ backgroundColor: "blue" }}
+        onPress={() => navigation.replace("Login")}
       >
-          <Text style={{color:'white'}} >Go to Login</Text>
+        <Text style={{ color: "white" }}>Go to Login</Text>
       </TouchableOpacity>
-
     </View>
-   ) 
-}  
-
+  );
+};
 
 const DetailScreen = () => {
-  const navigation = useNavigation(); 
-  return(
-   <View>
-         <Text>Man hinh Detail</Text>
-         <TouchableOpacity style={{backgroundColor:'blue'}}
-          onPress={()=> navigation.replace('Login')}
-     >
-         <Text style={{color:'white'}} >Go to Login</Text>
-     </TouchableOpacity>
-
-   </View>
-  ) 
-}  
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Text>Man hinh Detail</Text>
+      <TouchableOpacity
+        style={{ backgroundColor: "blue" }}
+        onPress={() => navigation.replace("Login")}
+      >
+        <Text style={{ color: "white" }}>Go to Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const InfoScreen = () => {
-  const navigation = useNavigation(); 
-  return(
-   <View>
-         <Text>Man hinh Info</Text>
-         <TouchableOpacity style={{backgroundColor:'blue'}}
-          onPress={()=> navigation.replace('Login')}
-     >
-         <Text style={{color:'white'}} >Go to Login</Text>
-     </TouchableOpacity>
-
-   </View>
-  ) 
-}  
-
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Text>Man hinh Info</Text>
+      <TouchableOpacity
+        style={{ backgroundColor: "blue" }}
+        onPress={() => navigation.replace("Login")}
+      >
+        <Text style={{ color: "white" }}>Go to Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const Tab = createBottomTabNavigator();
-export default function Home() { 
-  return (         
-     <Tab.Navigator 
-     screenOptions={({ route }) => ({
-      tabBarIcon: ({ focused, color, size }) => {
-        let iconName;
+export default function Home() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-        if (route.name === 'HomeScreen') {
-          iconName = focused
-            ? 'ios-home'
-            : 'ios-home-outline';
-        } else if (route.name === 'DetailScreen') {
-          iconName = focused ? 'ios-list' : 'ios-list-outline';
-        } else if (route.name === 'InfoScreen') {
-        iconName = focused ? 'information-circle' : 'information-circle-outline';
-        }
+          if (route.name === "HomeScreen") {
+            iconName = focused ? "ios-home" : "ios-home-outline";
+          } else if (route.name === "DetailScreen") {
+            iconName = focused ? "ios-list" : "ios-list-outline";
+          } else if (route.name === "InfoScreen") {
+            iconName = focused
+              ? "information-circle"
+              : "information-circle-outline";
+          }
 
-        // You can return any component that you like here!
-        return <Ionicons name={iconName} size={size} color={color} />;
-      },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-          headerShown:false
-    })}>
-
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+        headerShown: false,
+      })}
+    >
       <Tab.Screen name="HomeScreen" component={HomeScreen} />
       <Tab.Screen name="DetailScreen" component={DetailScreen} />
       <Tab.Screen name="InfoScreen" component={InfoScreen} />
@@ -89,8 +86,8 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- Screens/Login.js
+++ Screens/Login.js
@@ -1,32 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { TouchableOpacity } from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, TextInput, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
+const Header = () => {
+  return (
+    <View style={styles.hd}>
+      <Text style={styles.hd_text}>Đăng nhập</Text>
+    </View>
+  );
+};
 
-export default function Login() {  
+const Body = () => {
+  return (
+    <View style={styles.bd}>
+      <Text style={styles.hd_text}>Body</Text>
+      <View style={{ paddingHorizontal: 15 }}>
+        <TextInput
+          style={{ backgroundColor: "#fff", height: 50, fontSize: 17 }}
+          placeholder="tên đăng nhập"
+        ></TextInput>
+        <TextInput
+          style={{ backgroundColor: "#fff", height: 50, fontSize: 17 }}
+          placeholder="tên đăng nhập"
+        ></TextInput>
+      </View>
+    </View>
+  );
+};
+
+const Footer = () => {
+  return (
+    <View style={styles.ft}>
+      <Text style={styles.hd_text}>Footer</Text>
+    </View>
+  );
+};
+
+export default function Login() {
   const navigation = useNavigation();
 
   return (
-    <View style={styles.container}>
-    <StatusBar style="auto" />
-      <Text>Man hinh Login</Text>      
-
-      <TouchableOpacity style={{backgroundColor:'gray'}}
-        onPress={()=> navigation.replace('Home')}
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto" />
+      <Header />
+      <Body />
+      <Footer />
+      <TouchableOpacity
+        style={{ backgroundColor: "gray" }}
+        onPress={() => navigation.replace("Home")}
       >
-         <Text style={{color:'white'}} >Go to Home</Text>
+        <Text style={{ color: "white" }}>Go to Home</Text>
       </TouchableOpacity>
-
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    //alignItems: "center",
+    //justifyC paddinv
+  },
+  hd: {
+    flex: 1,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  hd_text: {
+    fontSize: 30,
+    fontWeight: 700,
+  },
+
+  bd: {
+    flex: 2,
+    backgroundColor: "gray",
+  },
+
+  ft: {
+    flex: 1,
+    backgroundColor: "white",
   },
 });
